f1ea7b39c94724e87de293b2264a1e1da639c41e
Roman Klochkov <kalimehtar@mail.ru>
2019-04-29 08:32:58 +0500

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang setup/infotab
 (define version "0.50")
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "hive-common"))
+(define deps '("base" "scribble-lib" "hive-common" "rackunit-lib" "srfi-lib"))
 (define build-deps '("racket-doc"))
